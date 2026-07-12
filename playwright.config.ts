b9fe2d7bba48d52fabd8942bc21600da58ff4322
kuker24 <34875577+kuker24@users.noreply.github.com>
2026-07-12 11:03:43 +0700

    fix: make Playwright config strict-type safe

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: Boolean(process.env.CI),
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : "50%",
   reporter: process.env.CI ? "github" : "html",
   use: {
     baseURL: "http://127.0.0.1:3000",
